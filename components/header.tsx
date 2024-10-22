693907ae36b4aab7cc47e94ba3c4399231a4d2f5
kasperstorgaard <kasper.storgaard@gmail.com>
2024-10-22 19:58:43 +0200

    feature(components): add simplified header

---
+++ components/header.tsx
@@ -0,0 +1,27 @@
+type Props = {
+  items: {
+    name: string;
+    href: string;
+  }[];
+};
+
+export function Header({ items }: Props) {
+  return (
+    <header className="mb-2">
+      <nav className="flex items-center gap-1 text-fl-0">
+        {items.map((item, idx) => (
+          <>
+            {idx > 0 && <span className="leading-0 mb-00">/</span>}
+
+            <a
+              href={item.href}
+              className="underline text-link"
+            >
+              {item.name}
+            </a>
+          </>
+        ))}
+      </nav>
+    </header>
+  );
+}
