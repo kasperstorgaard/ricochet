39dd8f62a292f446cd57a43fe509faeb40646af7
kasperstorgaard <kasper.storgaard@gmail.com>
2026-02-06 10:31:54 +0100

    add tutorial icons

--- islands/tutorial-dialog.tsx
+++ islands/tutorial-dialog.tsx
@@ -124,16 +124,17 @@ function TutorialPiecesStep({ href }: TutorialStepProps) {
       <div className="flex flex-col gap-fl-2 text-text-2">
         <h1 className="text-fl-2 leading-tight text-text-1">The pieces</h1>
         <p>
-          There are two pieces: the "rook" and the "bouncers".
+          There are two pieces: the rook (<IconRook />) and the bouncer{" "}
+          (<IconBouncer />).
         </p>
 
         <p>
           Both pieces move like a chess rook, but <strong>only</strong>{" "}
-          stop when they hit each other or a wall.
+          stop when they hit each other or a wall (<IconWall />).
         </p>
         <p>
-          When you get the rook to stop on the target, you have found a
-          solution.
+          When you get the rook (<IconRook />) to stop on the target{" "}
+          (<IconDestination />), you have found a solution.
         </p>
       </div>
 
@@ -150,7 +151,6 @@ function TutorialPiecesStep({ href }: TutorialStepProps) {
           href={nextStep}
           className="btn"
           data-router="push"
-          onClick={globalThis.location.reload}
         >
           Show me!
         </a>
@@ -191,8 +191,10 @@ function TutorialSolutionStep({ href }: TutorialStepProps & {
         </p>
 
         <p>
-          You can see the number of moves in the control panel, where you can
-          also undo/redo moves and reset the puzzle.
+          You can see the number of moves in the control panel{" "}
+          <span className="max-lg:hidden">on the right</span>
+          <span className="lg:hidden">below</span>, where you can also undo/redo
+          moves and reset the puzzle.
         </p>
       </div>
 
@@ -235,4 +237,55 @@ function getStepLink(
   if (replaySpeed) url.searchParams.set("replay_speed", replaySpeed.toString());
 
   return url.href;
+}
+
+function IconRook() {
+  return (
+    <svg viewBox="0 0 50 50" className="inline w-5 h-5 align-middle">
+      <circle cx="25" cy="25" r="20" fill="var(--color-ui-2)" />
+    </svg>
+  );
+}
+
+function IconBouncer() {
+  return (
+    <svg viewBox="0 0 50 50" className="inline w-5 h-5 align-middle">
+      <rect
+        x="5"
+        y="5"
+        width="40"
+        height="40"
+        rx="6"
+        ry="6"
+        fill="var(--color-ui-3)"
+      />
+    </svg>
+  );
+}
+
+function IconDestination() {
+  return (
+    <svg viewBox="0 0 50 50" className="inline w-5 h-5 align-middle">
+      <g stroke="var(--color-ui-1)" strokeWidth="3" fill="none">
+        <rect x="5" y="5" width="40" height="40" />
+        <line x1="5" y1="5" x2="45" y2="45" />
+        <line x1="5" y1="45" x2="45" y2="5" />
+      </g>
+    </svg>
+  );
+}
+
+function IconWall() {
+  return (
+    <svg viewBox="0 0 10 50" className="inline w-2 h-5 align-middle">
+      <line
+        x1="5"
+        y1="5"
+        x2="5"
+        y2="45"
+        strokeWidth="6"
+        stroke="var(--color-ui-4)"
+      />
+    </svg>
+  );
 }
