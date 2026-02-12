a4735502d84e822f9355afc0c3fb218af8faa0a8
kasperstorgaard <kasper.storgaard@gmail.com>
2026-02-12 11:11:57 +0100

    add puzzles level middleware

---
+++ routes/puzzles/_middleware.ts
@@ -0,0 +1,12 @@
+import { define } from "#/core.ts";
+import { getDifficultyBadgeFlag } from "#/lib/feature-flags.ts";
+
+const middleware = define.middleware(async (ctx) => {
+  ctx.state.featureFlags.difficultyBadge = await getDifficultyBadgeFlag(
+    ctx.state,
+  );
+
+  return await ctx.next();
+});
+
+export default [middleware];
