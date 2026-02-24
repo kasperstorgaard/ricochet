98db71663d5f019a4bbe86d70fb29e91923eee63
Kasper Storgaard <kasper.storgaard@gmail.com>
2026-02-24 14:27:48 +0100

    tweak the spacing when printing

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -54,7 +54,7 @@ export default define.page(
             "sm:grid-cols-[minmax(var(--size-fluid-3),auto)_max-content_minmax(var(--size-fluid-3),auto)]",
             "grid-rows-[repeat(3,max-content)_1fr] place-items-[flex-end_center] gap-y-fl-3",
             "lg:grid-cols-[1fr_13rem] lg:content-center",
-            "print:grid-cols-[1fr_max-content_1fr] print:grid-rows-[auto_auto] print:gap-0",
+            "print:grid-cols-[1fr_max-content_1fr] print:grid-rows-[auto_auto] print:gap-fl-1",
           )}
         >
           <Component />
