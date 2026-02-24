b979bdaa0ba8a82a9b79c77979bd0d382631c128
Kasper Storgaard <kasper.storgaard@gmail.com>
2026-02-24 08:58:24 +0100

    tweak the panel min height on mobile

--- components/panel.tsx
+++ components/panel.tsx
@@ -16,7 +16,7 @@ export function Panel({ children, className }: PanelProps) {
   return (
     <aside
       className={clsx(
-        "col-span-3 grid grid-cols-subgrid place-content-start max-md:min-h-[min(25vh,20rem)]",
+        "col-span-3 grid grid-cols-subgrid place-content-start max-md:min-h-[min(30dvh,20rem)]",
         "border-t-2 border-brand bg-surface-2 text-fl-1 py-fl-3",
         "lg:col-start-2 lg:col-span-1 lg:grid-rows-subgrid lg:row-span-full lg:px-fl-2",
         className,
