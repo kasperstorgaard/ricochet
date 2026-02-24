4e44c7324d1d68f6c8f5b5abb4f3483da60d27fd
Kasper Storgaard <kasper.storgaard@gmail.com>
2026-02-24 20:16:17 +0100

    adjust print layout

--- components/print-panel.tsx
+++ components/print-panel.tsx
@@ -2,7 +2,7 @@ import { Panel } from "#/components/panel.tsx";
 
 export function PrintPanel() {
   return (
-    <Panel className="not-print:hidden bg-transparent pt-fl-0 border-none mb-fl-3">
+    <Panel className="not-print:hidden bg-transparent pt-fl-0 border-none mb-fl-5">
       <div className="col-[2/3] grid grid-cols-2 gap-fl-3">
         {[0, 1].map((col) => (
           <div key={col} className="flex flex-col gap-fl-3">
@@ -10,10 +10,10 @@ export function PrintPanel() {
               { length: 6 },
               (_, row) => (
                 <div key={row} className="flex gap-fl-1 items-end">
-                  <span className="border-b border-text-3 px-fl-1 py-fl-1 w-8 font-mono text-00 leading-tight text-text-3/40">
+                  <span className="border-b border-text-3 pl-2 py-fl-1 w-8 font-mono text-00 leading-tight text-text-3/70">
                     {row === 0 ? "moves" : ""}
                   </span>
-                  <span className="border-b border-text-3 flex-1 px-fl-1 py-fl-1 font-mono text-00 leading-tight text-text-3/40">
+                  <span className="border-b border-text-3 flex-1 pl-2 py-fl-1 font-mono text-00 leading-tight text-text-3/70">
                     {row === 0 ? "name" : ""}
                   </span>
                 </div>
